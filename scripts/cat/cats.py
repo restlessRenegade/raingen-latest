1a85a1dd102982b96a7a2c8dee70aad22c6cab59
flamedash2358 <lucykooistra@icloud.com>
2025-04-30 12:01:04 -0700

    Update cats.py

--- scripts/cat/cats.py
+++ scripts/cat/cats.py
@@ -90,12 +90,18 @@ class Cat:
     # Ranges are inclusive to both bounds
     experience_levels_range = {
         "untrained": (0, 0),
-        "trainee": (1, 50),
-        "prepared": (51, 110),
-        "competent": (110, 170),
-        "proficient": (171, 240),
-        "expert": (241, 320),
-        "master": (321, 321),
+          "trainee": (1, 30),
+        "beginner": (31, 60),
+        "novice": (61, 90), 
+        "prepared": (91, 130),
+        "competent": (131, 170),
+        "skilled": (171, 210),
+        "proficient": (211, 250),
+        "advanced": (251, 290), 
+        "expert": (291, 330),
+        "adept": (331, 370), 
+        "master": (371, 410),
+        "grandmaster": (411, 411)
     }
 
     default_pronouns = [
@@ -2856,14 +2862,28 @@ class Cat:
         elif mediator.experience_level == "trainee":
             # Negative bonus for very low.
             chance = 20
+         elif mediator.experience_level == "beginner":
+            chance = 25
+        elif mediator.experience_level == "novice":
+            chance = 30
         elif mediator.experience_level == "prepared":
             chance = 35
+        elif mediator.experience_level == "competent":
+            chance = 40
+        elif mediator.experience_level == "skilled":
+            chance = 45
         elif mediator.experience_level == "proficient":
             chance = 55
+        elif mediator.experience_level == "advanced":
+            chance = 60
         elif mediator.experience_level == "expert":
+            chance = 65
+        elif mediator.experience_level == "adept":
             chance = 70
         elif mediator.experience_level == "master":
-            chance = 100
+            chance = 90
+         elif mediator.experience_level == "grandmaster":
+            chance = 99
         else:
             chance = 40
 
@@ -3340,7 +3360,7 @@ class Cat:
 
     @experience.setter
     def experience(self, exp: int):
-        exp = min(exp, self.experience_levels_range["master"][1])
+        exp = min(exp, self.experience_levels_range["grandmaster"][1])
         self._experience = int(exp)
 
         for x in self.experience_levels_range:
@@ -3473,6 +3493,9 @@ class Cat:
                 "opacity": self.pelt.opacity,
                 "prevent_fading": self.prevent_fading,
                 "favourite": self.favourite,
+                "fur_texture": self.pelt.fur_texture,
+                "height": self.pelt.height,
+                "build": self.pelt.build,
             }
 
     def determine_next_and_previous_cats(self, status: List[str] = None, exclude_status: List[str] = None):
