931266825a33b96d0d028dc63f2892e9e8b535d5
flamedash <lucykooistra@icloud.com>
2025-10-19 11:31:52 -0700

    adding ur platforms

--- scripts/screens/ProfileScreen.py
+++ scripts/screens/ProfileScreen.py
@@ -3260,7 +3260,7 @@ class ProfileScreen(Screens):
             "resources/images/platforms.png"
         ).convert_alpha()
 
-        order = ["beach", "forest", "mountainous", "nest", "plains", "SC/DF"]
+        order = ["beach", "forest", "mountainous", "nest", "plains", "SC/DF/UR"]
 
         biome_platforms = platformsheet.subsurface(
             pygame.Rect(0, order.index(biome) * 70, 640, 70)
@@ -3276,7 +3276,7 @@ class ProfileScreen(Screens):
 
         if the_cat.df:
             biome_platforms = platformsheet.subsurface(
-                pygame.Rect(0, order.index("SC/DF") * 70, 640, 70)
+                pygame.Rect(0, order.index("SC/DF/UR") * 70, 640, 70)
             )
             return pygame.transform.scale(
                 biome_platforms.subsurface(pygame.Rect(0 + offset, 0, 80, 70)),
@@ -3284,10 +3284,18 @@ class ProfileScreen(Screens):
             )
         elif the_cat.dead or game.clan.instructor.ID == the_cat.ID:
             biome_platforms = platformsheet.subsurface(
-                pygame.Rect(0, order.index("SC/DF") * 70, 640, 70)
+                pygame.Rect(0, order.index("SC/DF/UR") * 70, 640, 70)
             )
             return pygame.transform.scale(
                 biome_platforms.subsurface(pygame.Rect(160 + offset, 0, 80, 70)),
+                (240, 210),
+            )
+        elif the_cat.dead and the_cat.outside:
+            biome_platforms = platformsheet.subsurface(
+                pygame.Rect(0, order.index("SC/DF/UR") * 70, 640, 70)
+            )
+            return pygame.transform.scale(
+                biome_platforms.subsurface(pygame.Rect(320 + offset, 0, 80, 70)),
                 (240, 210),
             )
         else:
